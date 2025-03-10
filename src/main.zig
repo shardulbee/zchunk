8feb318d240f4919724ea554dda014ea733ac3cc
Shardul Baral <shardul@baral.ca>
2025-03-09 21:36:31 -0400

    angela merkle

--- src/main.zig
+++ src/main.zig
@@ -36,9 +36,7 @@ const Args = struct {
     n_threads: u8 = 1,
 };
 
-const ArgsType = union(enum) { help: bool, file: Args, stdout: Args };
-
-fn parse_args(args: *std.process.ArgIterator, input_fname_buf: [*]u8, output_fname_buf: [*]u8) ArgsType {
+fn parse_args(args: *std.process.ArgIterator, input_fname_buf: [*]u8, output_fname_buf: [*]u8) ?Args {
     _ = args.skip(); // skip executable
     var mode: Mode = Mode.summary;
     var hash: Hash = Hash.sha256;
@@ -51,7 +49,7 @@ fn parse_args(args: *std.process.ArgIterator, input_fname_buf: [*]u8, output_fna
 
     while (args.next()) |arg| {
         if (std.mem.eql(u8, arg, "--help")) {
-            return ArgsType{ .help = true };
+            return null;
         }
         var split = std.mem.splitSequence(u8, arg, "=");
         if (std.mem.eql(u8, split.peek().?, arg)) {
@@ -69,7 +67,7 @@ fn parse_args(args: *std.process.ArgIterator, input_fname_buf: [*]u8, output_fna
                 mode = Mode.chunks;
             } else {
                 std.debug.print("Unrecognized mode provided: {s}\n\n", .{mode_str});
-                return ArgsType{ .help = true };
+                return null;
             }
         } else if (std.mem.eql(u8, flag, "--hash")) {
             const hash_str = split.next().?;
@@ -81,7 +79,7 @@ fn parse_args(args: *std.process.ArgIterator, input_fname_buf: [*]u8, output_fna
                 //     hash = hash_kind.xxhash;
             } else {
                 std.debug.print("Unrecognized hashing function provided: {s}\n\n", .{hash_str});
-                return ArgsType{ .help = true };
+                return null;
             }
         } else if (std.mem.eql(u8, flag, "--chunk-size")) {
             const chunk_size_str = split.next().?;
@@ -117,7 +115,7 @@ fn parse_args(args: *std.process.ArgIterator, input_fname_buf: [*]u8, output_fna
             };
         } else {
             std.debug.print("Unrecognized flag provided: {s}\n\n", .{arg});
-            return ArgsType{ .help = true };
+            return null;
         }
     }
 
@@ -131,20 +129,15 @@ fn parse_args(args: *std.process.ArgIterator, input_fname_buf: [*]u8, output_fna
         in_file = input_fname_buf[0..fname.len];
     } else {
         std.debug.print("Expected a filename.\n\n", .{});
-        return ArgsType{ .help = true };
+        return null;
     }
 
+    var return_args = Args{ .mode = mode, .n_threads = n_threads, .hash = hash, .chunk_size = chunk_size, .in_fname = in_file.? };
     if (out_file) |f| {
-        return ArgsType{ .file = Args{ .mode = mode, .n_threads = n_threads, .hash = hash, .chunk_size = chunk_size, .out_fname = f, .in_fname = in_file.? } };
-    } else {
-        return ArgsType{ .stdout = Args{ .mode = mode, .n_threads = n_threads, .hash = hash, .chunk_size = chunk_size, .in_fname = in_file.? } };
+        return_args.out_fname = f;
     }
 
-    return ArgsType{ .help = true };
-}
-
-fn handle_file(file_args: Args) void {
-    std.debug.print("Outputting to a file: {?}\n\n", .{file_args});
+    return return_args;
 }
 
 fn handle_read_error(err: stdfile.ReadError) noreturn {
@@ -177,6 +170,46 @@ fn handle_open_error(err: stdfile.OpenError, path: []const u8) noreturn {
 fn print_and_exit_with_error(comptime msg: []const u8, args: anytype) noreturn {
     std.debug.print(msg, args);
     exit(1);
+}
+
+fn construct_merkle_tree(hashes: []u8, allocator: std.mem.Allocator) []const u8 {
+    // the rough strategy:
+    // - we're given bunch of hashes which correspond to each chunk of the file
+    // - these hashes are the leaves of the tree
+    // - since it is a binary tree, we can start with the last 2 leaves, concatenate them, and then hash them again
+    // - this becomes the parent of the 2 leaves. we store this in the index before the first leaf
+    // - then we repeat this process until we reach the first non-leaf and recursively do this for the next level
+    // to get the index of the parent, we take the index of the leaf, subtract 1, and divide by 2 (and take the floor but that's the default)
+
+    // a tree with N leaves contains n-1 internal nodes
+    // so the whole tree has 2N-1 nodes
+    const n_leaves = hashes.len / 32;
+    const tree_size = 2 * n_leaves - 1;
+    var tree = allocator.alloc(u8, tree_size * 32) catch print_and_exit_with_error("Unable to allocate memory for tree", .{});
+
+    // the leaves are at the end of the tree, copy them over
+    @memcpy(tree[(tree_size - n_leaves) * 32 ..], hashes);
+
+    // now we hash 64 bytes bytes from the end of the tree to the beginning, and store that at the parent index
+    // we repeat this until we're at the beginning
+    //
+    // NOTE: this means in the case of an odd number of leaves, the leftmost leaf will not have a sibling
+    // this is fine because it will be paired with an internal node from the level above
+    // for example, with 3 leaves [A, B, C]:
+    // 1. we hash B+C and store it at position 1
+    // 2. then we hash A+hash(B+C) and store it at position 0
+    // So we are "promoting" the odd leaf to the next level
+    var idx: usize = tree_size - 1;
+    while (idx > 0) {
+        const parent_idx = (idx - 1) / 2;
+        const start_idx = (idx - 1) * 32;
+        const end_idx = start_idx + 64;
+
+        sha2.Sha256.hash(tree[start_idx..end_idx], @ptrCast(tree[parent_idx * 32 .. (parent_idx + 1) * 32]), .{});
+
+        idx -= 2;
+    }
+    return tree;
 }
 
 fn compute_hashes(args: Args, allocator: std.mem.Allocator) []u8 {
@@ -224,6 +257,9 @@ fn handle_stdout(stdout_args: Args, allocator: std.mem.Allocator) void {
     const hashes = compute_hashes(stdout_args, allocator);
     defer allocator.free(hashes);
 
+    const merkle_tree = construct_merkle_tree(hashes, allocator);
+    defer allocator.free(merkle_tree);
+
     std.debug.print("File: {s}\n", .{stdout_args.in_fname});
     std.debug.print("Chunk\tOffset\tHash\n", .{});
     for (0..nchunks) |chunk_idx| {
@@ -233,6 +269,12 @@ fn handle_stdout(stdout_args: Args, allocator: std.mem.Allocator) void {
         }
         std.debug.print("\n", .{});
     }
+
+    std.debug.print("Overall hash: \t", .{});
+    for (merkle_tree[0..32]) |byte| {
+        std.debug.print("{x:0>2}", .{byte});
+    }
+    std.debug.print("\n", .{});
 }
 
 fn work(nchunks: u64, thread_idx: usize, args: Args, hashes: []u8, allocator: std.mem.Allocator) void {
@@ -262,12 +304,13 @@ pub fn main() !void {
     var args = std.process.args();
     var input_fname_buf: [255]u8 = [_]u8{0} ** 255;
     var output_fname_buf: [255]u8 = [_]u8{0} ** 255;
-    const zcargs = parse_args(&args, &input_fname_buf, &output_fname_buf);
+    const parsed_args = parse_args(&args, &input_fname_buf, &output_fname_buf);
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    switch (zcargs) {
-        ArgsType.help => std.debug.print("{s}\n\n", .{help_text}),
-        ArgsType.file => handle_file(zcargs.file),
-        ArgsType.stdout => handle_stdout(zcargs.stdout, allocator),
+
+    if (parsed_args) |a| {
+        handle_stdout(a, allocator);
+    } else {
+        std.debug.print("{s}\n\n", .{help_text});
     }
 }
